226eb963b50f6fd44471544845368f1ee13a7b66
BogdanPopov <bogdanpopov@yandex.ru>
2020-01-22 17:06:18 +0500

    comments

--- Menu.py
+++ Menu.py
@@ -4,15 +4,15 @@ from pygame.sprite import Group
 
 from pygame.locals import *
 
-# Инициализируем pygame
+# Initialization of pygame.
 pygame.init()
-# Создаём игровое окно 800*570
+# Creating gaming window 800*570.
 
 screen_size = width, height = (800, 570)
 window = pygame.display.set_mode(screen_size)
-# Ставим свой заголовок окна
+# Creating window's title.
 pygame.display.set_caption('TETRIS')
-# иконка игры
+# icon of game.
 icon = pygame.image.load('data/tetris1.jpg')
 pygame.display.set_icon(icon)
 FPS = 50
@@ -23,17 +23,17 @@ vertical_borders = pygame.sprite.Group()
 
 
 def load_image(name, colorkey=None):
-    # Добавляем к имени картинки имя папки
+    # adding the folder name to the image name.
     fullname = os.path.join('data', name)
-    # Загружаем картинку
+    # loading the picture.
     try:
         image = pygame.image.load(fullname)
     except pygame.error as message:
-        print('Не удаётся загрузить:', name)
+        print('Cannot load:', name)
         raise SystemExit(message)
     image = image.convert()
-    # Если второй параметр =-1 делаем прозрачным
-    # цвет из точки 0,0
+    # If second setting =-1: doing transparent.
+    # color from point 0,0.
     if colorkey is not None:
         if colorkey is -1:
             colorkey = image.get_at((0, 0))
@@ -47,18 +47,18 @@ def terminate():
 
 
 def start_screen():
-    # загружаем фон
+    # loading background.
     fon, b = load_image('tetris1.jpg')
-    # загружаем музыку
+    # loading music.
     # pygame.mixer.music.load('data/zvuk-vstuplenija-v-tetrise-na-dendi.mp3')
-    # pygame.mixer.music.set_volume(0.3)  # 1 -100%  громкости звука
-    # pygame.mixer.music.play(-1)  # играть бесконечно -1
+    # pygame.mixer.music.set_volume(0.3)  # 1 -100%  громкости звука.
+    # pygame.mixer.music.play(-1)  # играть бесконечно -1.
 
-    # Создание кнопки Play.
+    # Creation the button 'Play'.
     play_button = Button(280, 70, window)
-    # Создание кнопки Показать правила
+    # Creation the button 'Rules'.
     rules_button = Button(280, 70, window)
-    # кнопка завершить игру
+    # Creating the button 'Quit'.
     quit_btn = Button(280, 70, window)
     menu = True
 
@@ -81,27 +81,27 @@ def start_screen():
 
 
 def check_play_button(button, mouse_x, mouse_y):
-    """Запускает новую игру при нажатии кнопки Play."""
+    """Launch a new game by pressing the button 'Play'."""
     if button.rect.collidepoint(mouse_x, mouse_y):
         pass
         start_game()
 
 
 def check_rules_button(button, mouse_x, mouse_y):
-    """Вызывает экран с правилами игры"""
+    """Calling the screen with game's rules."""
     if button.rect.collidepoint(mouse_x, mouse_y):
         rules_show()
 
 
 def rules_show():
-    """экран с правилами игры"""
-    intro_text = ["Цель: как можно дольше продержаться в игре", "",
-                  "Собрать как можно больше очков ", "",
-                  "Скорость появления кубиков тетрамино с каждым уровнем растет", "",
-                  "Стараться заполнить ряд", "",
-                  "Можно поворачивать фигурки", "",
-                  "Можно собственноручно увеличивать скорость падения фигур", "",
-                  "Для паузы в игре нажмите ESC"]
+    """Screen with game's rules."""
+    intro_text = ["The aim: stay in the game as long as possible.", "",
+                  "Collect so many points as possible.", "",
+                  "The rate of appearance of 'tetramino' cubes increases with each level.", "",
+                  "Do all your best to fill the raw.", "",
+                  "You can turn the figures.", "",
+                  "You can personally increase the speed of falling figures.", "",
+                  "To pause the game press ESC."]
 
     fon, b = load_image('tetris2.jpg')
     window.blit(fon, (0, 0))
@@ -128,7 +128,7 @@ def rules_show():
 
 
 def check_quit_button(button, mouse_x, mouse_y):
-    """Выход из игры."""
+    """Leave the game."""
     button_clicked = button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked:
         terminate()
@@ -136,10 +136,10 @@ def check_quit_button(button, mouse_x, mouse_y):
 
 def start_game():
     global cycle
-    # добавляем музыку - только mp3
+    # Adding the music - only mp3.
     # pygame.mixer.music.load('data/space_game.mp3')
-    # pygame.mixer.music.set_volume(0.3)  # 1 -100%   звука
-    # pygame.mixer.music.play(-1)  # играть бесконечно -1, либо число означаюшее кличество циклов проигрывания
+    # pygame.mixer.music.set_volume(0.3)  # 1 -100% sound
+    # pygame.mixer.music.play(-1)  # play unlimited -1, either a number indicates the amount of payback cycles.
     cycle = True
 
     while cycle:
@@ -148,12 +148,12 @@ def start_game():
 
 def print_text(x, y, msg, font_color=(0, 0, 0), font_type='data/PingPong.ttf', font_size=50):
     """
-    вывод текста на кнопке
-    :param x:  координата начала сообщения по х
-    :param y: координата начала сообщения по у
-    :param font_color: цвет шрифта
-    :param font_type: тип  шрифта
-    :param font_size: размер шрифта
+    Output the text from the button.
+    :param x:  coordinate of starting the message on х.
+    :param y: coordinate of starting the message on у.
+    :param font_color: font color.
+    :param font_type: font type.
+    :param font_size: font size.
     :return:
     """
     font_type = pygame.font.Font(font_type, font_size)
@@ -164,15 +164,15 @@ def print_text(x, y, msg, font_color=(0, 0, 0), font_type='data/PingPong.ttf', f
 class Button:
     def __init__(self, width, heigth, screen):
         """
-        Инициализирует атрибуты кнопки.
-        :param width: -ширина кнопки
-        :param heigth: -высота
-        :param screen: -экран
+        Initialization the buttons's attributes.
+        :param width: - buttons's width.
+        :param heigth: -height.
+        :param screen: -screen.
         """
         self.screen = screen
         self.screen_rect = screen.get_rect()
 
-        # Назначение размеров и свойств кнопок.
+        # Purpose of sizes and functions of the buttons.
         self.width, self.heigth = width, heigth
         self.button_color = (0, 219, 106)
 
@@ -180,7 +180,7 @@ class Button:
         self.font = pygame.font.SysFont(None, 48)
 
     def draw_button(self, x, y, msg):
-        # Отображение  кнопки
+        # Displaying the buttons.
         self.rect = pygame.Rect(x, y, self.width, self.heigth)
         pygame.draw.rect(self.screen, self.button_color, self.rect)  # (x, y, self.width, self.heigth))
         print_text(x + 90, y + 10, msg)
