12dda5c29141320578f7d6f491cf55b2c57f3582
tryharder-at <57711783+tryharder-at@users.noreply.github.com>
2020-01-29 17:09:21 +0500

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -218,7 +218,7 @@ def start_game() -> None:
     app = TetrisGame()
     app.run()
     # Adding the music - only mp3.
-    pygame.mixer.music.load('data/')
+    pygame.mixer.music.load('data/zvuk-tetrisa-na-konsoli.mp3')
     pygame.mixer.music.set_volume(0.3)  # 1 -100% sound
     pygame.mixer.music.play(-1)  # play unlimited -1, either a number indicates the amount of payback cycles.
 
